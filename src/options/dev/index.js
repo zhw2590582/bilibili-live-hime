2b065f44f580a3d8667175565ee3073330c51392
Harvey <laozhaochaguan@gmail.com>
2019-11-29 13:40:34 +0800

    添加选项页

---
+++ src/options/dev/index.js
@@ -0,0 +1,9 @@
+import './index.scss';
+
+class Options {
+    constructor() {
+        console.log('Options');
+    }
+}
+
+export default new Options();
---
+++ src/options/index.js
@@ -0,0 +1,23 @@
+var bilibiliLiveHimeOptions = (function () {
+  'use strict';
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  var classCallCheck = _classCallCheck;
+
+  var Options = function Options() {
+    classCallCheck(this, Options);
+
+    console.log('Options');
+  };
+
+  var index = new Options();
+
+  return index;
+
+}());
+//# sourceMappingURL=index.js.map
