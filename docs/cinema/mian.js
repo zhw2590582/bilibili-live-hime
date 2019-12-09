77bbfcda6d593cd5cb45c81baecd2b8923ac4b78
Harvey <laozhaochaguan@gmail.com>
2019-12-09 17:47:54 +0800

    添加点播厅演示

---
+++ docs/cinema/mian.js
@@ -0,0 +1,5 @@
+var url = 'https://zhw2590582.github.io/assets-cdn';
+var art = new Artplayer({
+    container: '.artplayer-app',
+    url: url + '/video/one-more-time-one-more-chance-480p.mp4',
+});
