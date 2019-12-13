8da508608d1c51649539084a41ab64f9b85dbc3c
Harvey <laozhaochaguan@gmail.com>
2019-12-13 18:25:08 +0800

    Update injected.js

--- injected.js
+++ injected.js
@@ -1,9 +1,5 @@
-var WS = window.WebSocket;
-window.WebSocket = function(arg) {
-  const ws = new WS(arg);
-  setTimeout(() => {
-    const ins = ws.onmessage({});
-    console.log(JSON.stringify(ins.options));
-  }, 0);
-  return ws;
-};
+const $script = document.createElement('script');
+$script.type = 'text/javascript';
+$script.text = `!function e(){if(window.DanmakuWebSocket){const e=window.DanmakuWebSocket;window.DanmakuWebSocket=function(n){const t=new e(n);return window.postMessage({type:"DANMU_OPTION",data:Object.keys(n).reduce((e,t)=>("function"!=typeof n[t]&&(e[t]=n[t]),e),{})}),t}}else setTimeout(e,10)}();`;
+document.documentElement.appendChild($script);
+$script.onload = document.documentElement.removeChild($script);
