5614ab464f0ca06b4b20fd070acb54841926f9eb
zhw2590582 <laozhaochaguan@gmail.com>
2019-12-20 22:47:02 +0800

    支持多FFmpeg进程

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,96 +1,58 @@
 const port = Number(process.argv[2]) || 8080;
 const io = require('socket.io')(port);
-const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
-const spawn = require('child_process').spawn;
+const FFmpeg = require('./FFmpeg');
 
-function createFFmpegProcess(rtmp) {
-    return spawn(ffmpegPath, [
-        '-re',
-        '-i',
-        '-',
-        '-vcodec',
-        'copy',
-        '-acodec',
-        'aac',
-        '-b:a',
-        '192k',
-        '-f',
-        'flv',
-        rtmp,
-    ]);
-}
-
-let ffmpeg = null;
 console.log(`中转地址：http://localhost:${port}`);
 
 io.on('connection', function(socket) {
     // 来自浏览器：开启ffmpeg进程
     socket.on('rtmp', rtmp => {
-        console.log('来自浏览器：开启ffmpeg进程');
-        
-        if (ffmpeg) {
-            try {
-                ffmpeg.stdin.end();
-                ffmpeg.kill('SIGINT');
-                ffmpeg = null;
-            } catch (err) {
-                console.log(err);
-            }
-        }
+        const ffmpeg = FFmpeg.getInstance(socket) || new FFmpeg(rtmp, socket);
 
-        ffmpeg = createFFmpegProcess(rtmp);
+        ffmpeg.close(code => {
+            ffmpeg.destroy();
 
-        // 告知浏览器：打印
-        socket.emit('log', '创建FFmpeg进程成功');
-
-        ffmpeg.stdout.on('data', data => {
-            console.log(String(data));
-        });
-
-        ffmpeg.stderr.on('data', data => {
-            console.log(String(data));
-        });
-
-        ffmpeg.on('close', code => {
-            // 告知浏览器：重连
             const msg = 'FFmpeg进程退出：' + code;
+
+            // 告知浏览器：重连
             socket.emit('reconnect', msg);
+
             // 告知浏览器：终止
             // socket.emit('fail', msg);
+
             console.log(msg);
         });
+
+        // 告知浏览器：打印
+        socket.emit('log', `创建FFmpeg进程成功: ${FFmpeg.instances.size}`);
     });
 
     // 来自浏览器：推流
     socket.on('binary_stream', data => {
+        const ffmpeg = FFmpeg.getInstance(socket);
         if (ffmpeg) {
-            ffmpeg.stdin.write(data);
+            ffmpeg.write(data);
         }
     });
 
     // 来自浏览器：终止
     socket.on('stream_disconnect', () => {
         console.log('来自浏览器：终止');
+        const ffmpeg = FFmpeg.getInstance(socket);
         if (ffmpeg) {
-            try {
-                ffmpeg.stdin.end();
-                ffmpeg.kill('SIGINT');
-                ffmpeg = null;
-            } catch (err) {
-                console.log(err);
-            }
+            ffmpeg.destroy();
         }
     });
 
-    socket.on('error', err => {
-        console.log(err);
+    socket.on('error', error => {
+        console.log(error);
     });
 });
 
-io.on('error', err => {
-    console.log(err);
+io.on('error', error => {
+    console.log(error);
 });
 
-process.on('uncaughtException', err => {
-    console.log(err);
+process.on('uncaughtException', error => {
+    console.log(error);
 });
